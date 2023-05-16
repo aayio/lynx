a576bb2a18d1a3627538ad90dffe6c0ff7918dc2
aayio <41933025+aayio@users.noreply.github.com>
2023-05-16 17:13:52 +0800

    revert to 0e8b87e

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     "./themes/lynx/layouts/**/*.html",
     "./themes/lynx/content/**/*.{html,md}",
   ],
-  darkMode: "class",
+  darkMode: "media",
   theme: {
     colors: {
       white: "#fff",
